e68a0d4e8d14c83f037f35673ba7953aee7e5854
Alex Puchades <alex94puchades@gmail.com>
2017-07-29 19:13:32 +0200

    Add check for false-ish values

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export type MaybePromise<T> = T | Promise<T>;
 export type MaybeAsyncFn<T> = () => Iterator<MaybePromise<T>>;
 
 function isPromise<T>(value: any): value is Promise<T> {
-	return value.then !== undefined;
+	return value && value.then !== undefined;
 }
 
 export function maybeAsync<T>(fn: MaybeAsyncFn<T>): MaybePromise<T> {
