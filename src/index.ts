0906f5076b2e4a7b7bac17b232911ef4816a77a9
Alex Puchades <alex94puchades@gmail.com>
2017-07-31 16:08:36 +0200

    Handle arrays of maybe-promised values

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,41 @@
 export type MaybePromise<T> = T | Promise<T>;
-
-export type MaybeAsyncFn<T> = () => Iterator<MaybePromise<T>>;
+export type MaybeAsyncFn<T> = () => MaybeAsyncIterator<any>;
+export type MaybeAsyncIterator<T> = Iterator<MaybePromise<T> | MaybePromise<T>[]>;
 
 function isPromise<T>(value: any): value is Promise<T> {
 	return value && value.then !== undefined;
 }
 
 export function maybeAsync<T>(fn: MaybeAsyncFn<T>): MaybePromise<T> {
-	const iter = fn();
 	let prev = undefined;
-	for (;;) {
+	for (const iter: MaybeAsyncIterator<T> = fn();;) {
 		const {value, done} = iter.next(prev);
-		if (done) return value;
-		if (isPromise(value)) return resumeAsyncFn(value, iter);
+		if (done)
+			return value as MaybePromise<T>;
+
+		if (isPromise(value))
+			return resumeAsyncFn(value, iter);
+
+		if (Array.isArray(value) && value.some(isPromise))
+			return resumeAsyncFn(Promise.all(value), iter);
+
 		prev = value;
 	}
 }
 
-async function resumeAsyncFn<T>(pending: Promise<T>, iter: Iterator<MaybePromise<T>>): Promise<T> {
-	let prev = await pending;
-	for (;;) {
+async function resumeAsyncFn<T>(value: MaybePromise<any>, iter: MaybeAsyncIterator<any>): Promise<T> {
+	for (let prev = await value;;) {
 		const {value, done} = iter.next(prev);
-		if (done) return value;
-		prev = await value;
+		if (done)
+			return value as Promise<T>;
+
+		if (isPromise(value)) {
+			prev = await value;
+		} else if (Array.isArray(value) && value.some(isPromise)) {
+			prev = await Promise.all(value);
+		} else {
+			prev = value;
+		}
 	}
 }
 
